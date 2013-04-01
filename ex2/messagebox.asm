ebbf61853bebe7209e16b7981c5d56db72a92cb5
Kibarski Jonathan <phenixdoc@gmail.com>
2013-04-01 22:08:11 +0300

    removed uneeded data section from messagebox.asm; more documentation

--- ex2/messagebox.asm
+++ ex2/messagebox.asm
@@ -1,10 +1,3 @@
-
-SECTION .data
-user32dll db "User32.dll",0x00
-msgBoxA db "MessageBoxA",0x00
-MB_CAPTION db "Hacked!",0x00
-MB_TEXT db "You've Been Hacked!",0x00
-
 SECTION .text
 global _main
 _main:
@@ -30,11 +23,11 @@ _main:
 	nop
 	sub esp, 0x110
 	push dword 0x0012FF26; User32.dll ; Stacked
-	mov eax, 0x7c801d7b; LoadLibraryA
+	mov eax, 0x7c801d7b; LoadLibraryA; fixed
 	call eax
 	push dword 0x0012FF32; MessageBoxA ; Stacked
 	push dword eax; LoadLibraryA return value
-	mov eax, 0x7c80ae2F; GetProcAddress
+	mov eax, 0x7c80ae2F; GetProcAddress; fixed
 	call eax
 	push dword 0; MB_OK
 	push dword 0x0012FF3F; Caption ; Stacked
