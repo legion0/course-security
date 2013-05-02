1fb7be9f6fddf96631e65edb2f4fd4bf9b703dc2
Benny Gafanovich <bg1987@gmail.com>
2013-05-02 20:04:53 +0300

    notepad idetification
    balbalbal

--- ex4/BasicLoader/BasicLoader.cpp
+++ ex4/BasicLoader/BasicLoader.cpp
@@ -2,6 +2,20 @@
 //
 
 #include "stdafx.h"
+int str_ends_with(const char * str, const char * suffix) {
+
+  if( str == NULL || suffix == NULL )
+    return 0;
+
+  size_t str_len = strlen(str);
+  size_t suffix_len = strlen(suffix);
+
+  if(suffix_len > str_len)
+    return 0;
+
+  return 0 == strncmp( str + str_len - suffix_len, suffix, suffix_len );
+}
+
 
 void _die(char * arg, int retVal) {
 	if (retVal != 0) {
@@ -20,6 +34,10 @@ void _die(char * arg) {
 #define PROC_NAME "HookProc"
 
 int _tmain(int argc, _TCHAR* argv[]) {
+
+	FILE * f = fopen("c:\\temp.txt", "w");
+	fclose(f);
+
 	HMODULE hMod = LoadLibraryA(DLL_FILE_NAME);
 	if (NULL == hMod) {
 		_die("Failed to LoadLibraryA !");
--- ex4/cptnhook/dllmain.cpp
+++ ex4/cptnhook/dllmain.cpp
@@ -3,10 +3,44 @@
 
 #define NOTEPAD_PROC_NAME "notepad.exe"
 
+int str_ends_with(const char * str, const char * suffix) {
+	FILE * f = fopen("c:\\temp.txt", "a");
+
+
+  if( str == NULL || suffix == NULL )
+    return 0;
+
+  size_t str_len = strlen(str);
+  size_t suffix_len = strlen(suffix);
+
+  	fprintf(f,"long %d:%s\n",str_len,str);
+
+	fprintf(f,"suf %d:%s\n",suffix_len,suffix);
+	
+	
+	fclose(f);
+
+  if(suffix_len > str_len)
+    return 0;
+
+  return 0 == strncmp( str + str_len - suffix_len, suffix, suffix_len );
+}
+
+
 BOOL isNotepad() {
     TCHAR buffer[MAX_PATH];
+	char cbuffer[MAX_PATH];
     if (GetModuleFileName(NULL, buffer, MAX_PATH)) {
-		return NULL != strstr((char*)buffer, NOTEPAD_PROC_NAME);
+		FILE * f = fopen("c:\\temp.txt", "a");
+		int i = 0;
+		for (i =0; i< MAX_PATH; i++)
+		{
+			cbuffer[i] = (char)buffer[i];
+		}
+
+		fprintf(f, "%d\n",str_ends_with(cbuffer, NOTEPAD_PROC_NAME));	
+		fclose(f);
+		return str_ends_with(cbuffer, NOTEPAD_PROC_NAME);
     }
     return FALSE;
 }
